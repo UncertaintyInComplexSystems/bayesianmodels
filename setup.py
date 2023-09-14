77c34199ede6bc36312c42dfec3d8f6e558ab46b
Max Hinne <mhinne@gmail.com>
2023-09-14 11:53:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import find_packages, setup
 
-import os
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
 setup(
     name='uicsmodels',
+    version='0.0.1',
     packages=find_packages(),
-    install_required=required,
+    install_requires=[
+        'jax',
+        'jaxtyping',
+        'distrax @ git+https://github.com/deepmind/distrax.git@f6e656c',
+        'jaxkern @ git+https://github.com/JaxGaussianProcesses/JaxKern.git',
+        'blackjax @ git+https://github.com/Hesterhuijsdens/blackjax.git'
+    ]
 )
