d5d26c38c3c4469ff53dffba159448a8d00c8f19
Max Hinne <mhinne@gmail.com>
2023-09-14 11:50:42 +0200

    Added requirements to setup

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import find_packages, setup
 
+import os
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='uicsmodels',
     packages=find_packages(),
+    install_required=required,
 )
