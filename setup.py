f2b703076400c8835ed88a56f41c827b7cc39190
Max Hinne <mhinne@gmail.com>
2023-09-14 16:39:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
-        'blackjax==0.9.6',
+        'blackjax @ git@github.com:Hesterhuijsdens/blackjax.git',
         'jaxkern @ git+https://github.com/JaxGaussianProcesses/JaxKern.git',
         'distrax==0.1.4'        
     ]
